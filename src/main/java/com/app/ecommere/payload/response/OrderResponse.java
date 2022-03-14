3ed6ce939eaad9d6b4187362ae94e42b96769ff2
Thinh Nguyen <anhthinh.61.got@gmail.com>
2022-03-14 08:30:55 +0700

    Add date response for order

--- src/main/java/com/app/ecommere/payload/response/OrderResponse.java
+++ src/main/java/com/app/ecommere/payload/response/OrderResponse.java
@@ -3,6 +3,7 @@ package com.app.ecommere.payload.response;
 import com.app.ecommere.model.OrderDTO;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -10,4 +11,5 @@ public class OrderResponse {
 
     private List<OrderDTO> products;
     private float total;
+    private Date date;
 }
